33038ca5680f8fc2a3a612c9cfd1baed78af1826
Aida Abdikulova <abdikulova@gmail.com>
2019-07-30 03:32:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 
   attr_reader :status
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
