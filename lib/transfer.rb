ea5cba3afffecc3ca192a39c72590b0f4d44077b
Aida Abdikulova <abdikulova@gmail.com>
2019-07-30 03:42:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,12 +21,8 @@ class Transfer
         @sender.balance -= @amount
         @receiver.deposit(@amount)
         @status = "complete"
-    elsif !@sender.valid?
-        @status = "rejected"
-        return "Transaction rejected. Please check your account balance."
-    else  @status = "rejected"
-        return "Transaction rejected. Please check your account balance."
-    end
+    else
+      puts "dog"
   end
 
 end
